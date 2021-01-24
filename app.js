f65543dfae03cd8edf2226f0c5594eb2afcd1e9a
faisal <faisal25march99@gmail.com>
2021-01-24 07:07:15 +0530

    Done with Hoisting

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 /*
 Introduction to Hoisting in JS
+-------------------------------
+
+Hositing is a phenomenon in Javascript by which you can access variables and functions, even before you have initialized it or you have put some value in it. 
 */
 
+getName() // Hello
+console.log(x); // undefined
+console.log(getName);
 
 var x = 7
 function getName(){
     console.log("Hello");
 }
-getName() // Hello
-console.log(x); // 7
