d18ce93472e1adc22f280a8f87a54801e69cc055
faisal <faisal25march99@gmail.com>
2021-04-03 12:37:25 +0530

    Example 2: Local Scope & Global Scope [ Scope and Lexical Environment ]

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 /*
 Scope and Lexical Environment
 */
+function a(){
+
+    c() 
+    var b = 10 //  phase I : undefiend --> Inside Local Scope of function a()
+   
+    function c() {
+        console.log(" I am inside C : " + b ); // I am inside C : undefined
+    }
+    
+}
+var b = 100 // Global Scope
 a()
 
-
-function a() {
-    console.log(b); // undefined
-
-}
-
-// Global Scope
-var b = 10
-
-
+//console.log(b); //  b is not defined
