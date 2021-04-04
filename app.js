e8187cc780d0a8ade38c1be52d82f5284b16a3ce
faisal <faisal25march99@gmail.com>
2021-04-04 08:37:21 +0530

    Day 3: Hoisting of let & const in JS

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-function a(){ // its lexical parent is global
-    var b = 10
-
-    c() 
-    function c(){ // its lexical parent is a
-        console.log(b); // 10
-
-        d()
-
-        function d() { // its lexical parent is c
-           // b = 20
-            console.log(b); // 20
-        }
-    }
-}
-
-a()
+console.log(x); // undefined 
+console.log(a); // ReferenceError: Cannot access 'a' before initialization
+var x = 10
+let a  = 20
