a5aed45607518c3de0c2f4be9c9ea8a70508b79d
faisal <faisal25march99@gmail.com>
2021-04-04 08:44:32 +0530

    let and const are allocated in Script Memory Space, not in Global Scope

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 console.log(x); // undefined 
-console.log(a); // ReferenceError: Cannot access 'a' before initialization
-var x = 10
-let a  = 20
+//console.log(a); // ReferenceError: Cannot access 'a' before initialization
+
+var x = 10 // created inside global
+let a  = 20 // created inside Script
+
+console.log(a);
+
+/*
+NOTE: # let and const allocate memory bt not in Global space
+
+# They allocate memory in spreate memory location called script
+
+# But in case of var the memory allocation happends in Global Space
+
+# Thats the reason you were facing ReferenceError: Cannot access 'a' before initialization
+
+# You have to perform declaration in let and const variables before accessing it
+*/
