48acaad2c6271d23d75b8d57583e5477a7c90ea3
faisal <faisal25march99@gmail.com>
2021-04-05 07:26:09 +0530

    Avoid Temporal Dead Zone

--- app.js
+++ app.js
@@ -1,27 +1,5 @@
-console.log(a); // ReferenceError: Cannot access 'a' before initialization --> In Temporal Dead Zone
-
-let a = 100
-var b = 200
-
-let greeting = () => {
-    console.log("hello");
-}
 /*
-Scope:
+How to avoid Temporal Dead Zone ?
 
-Script
-a: undefined
-
-
-Global               Window
-
-b: undefined
-
-
-Note: 
-# Temporal Dead Zone is a time since when this let variable was hoisted and till its initialied some value
-
-# The time between hoisitng and till its initialized some valuee its called Temporal Dead Zone
-
-# So the phase from Hoisitng tilll it assign or get some value that phase is known as Tempooral Deead Zone
+Always put  your Declaration and initialization on the top of your actual logic
 */
