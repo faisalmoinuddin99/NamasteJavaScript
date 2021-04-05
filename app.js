4bc8d92d37c4c28e53d6a71d3f9ff753b675cedd
faisal <faisal25march99@gmail.com>
2021-04-05 19:52:20 +0530

    Combined multiple JS statement into One Group

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-/*
-What is a BLOCK in JS ?
-
--> 1. BLOCK is also known as Compound Statements
-
-2. A Block is used to combined multiple JS statements into Group
-*/
-
 {
+    var a = 10
+    let b = 80
+    const pi = 3.14
 
+    console.log(a);
+    console.log(b);
+    console.log(pi);
 }
+
+console.log(a);
+console.log(b); // b is not defined
+console.log(pi);
+
+/*
+ * NOTE: A Block is used to combined multiple JavaScript statement into
+One Group 
+ */
